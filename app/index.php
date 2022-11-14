2051148b930eb20bda895b3453e8e6e28d3a9124
pcmagas <pcmagas@disroot.org>
2022-11-14 20:27:47 +0200

    Fix Connection.

--- app/index.php
+++ app/index.php
@@ -2,18 +2,17 @@
 
 $db_host = getenv("DB_HOST");
 $db_name = getenv("DB_NAME");
-$db_password = getenv("DB_PASSWD");
-$db_usr = getenv("DB_USR");
+$db_passwd = getenv("DB_PASSWD");
+$db_usr = getenv("DB_USER");
 $db_port = getenv("DB_PORT");
 
 $dsn="mysql:dbname=$db_name;host=$db_host:$db_port";
-
 try {
-    $pdo = new PDO($dsn,$db_host,$db_usr,$db_passwd);
+    $pdo = new PDO($dsn,$db_usr,$db_passwd);
     $sql = "SELECT COUNT(*) from mytable";    
     $stmt = $pdo->query($sql);
     $items = $stmt->fetch();
-    echo $items;
+    echo $items[0];
 } catch(Exception $e){
     echo "DB_ERROR";
 }
